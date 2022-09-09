b487187b4fd99472e88b33b2eebf9bec7f46d409
Abraq-SQA <johnabrak01@gmail.com>
2022-09-09 01:10:37 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints text
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome ia a piece of art, \n");
+	printf("with proper grammar, but the outcome ia a piece of art,\n");
 	Return (0);
 }
