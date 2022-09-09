3afb0d0aa0b0095391b68199fced15264bf214c6
Abraq-SQA <johnabrak01@gmail.com>
2022-09-09 08:52:20 -0700

    last digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- * main - print if the number is positive, zero, or negative
+ * main - print if the number is a positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative"
  * Return: 0
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+  /* my code goes here */
+if (n > 0)
 {
-	putchar(ch);
+	printf("%i is positive\n", n);
 }
-putchar('\n');
+else if (n == 0)
+{
+	printf("%i is zero\n", n);
+}
+else if (n < 0)
+{
+	printf("%i is positive\n", n);
+}
 return (0);
 }
