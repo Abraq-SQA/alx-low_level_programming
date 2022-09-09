ead1559af72bc3f13caa6487544c808a24b731b5
Abraq-SQA <johnabrak01@gmail.com>
2022-09-09 10:06:33 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,17 +4,18 @@
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative"
- * Return: 0
+ * Return 0
  */
 int main(void)
 {
-char c;
+char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if (ch != 'e' && ch != 'q')
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
 	putchar(ch);
 }
 putchar('\n');
 return (0);
-}
