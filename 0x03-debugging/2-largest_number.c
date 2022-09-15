a5c87ff32c4b660d231a07127e02c4efdeb5d6fb
Abraq-SQA <johnabrak01@gmail.com>
2022-09-15 10:07:35 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * main - print largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
-	int largest = b
+	int largest = b;
 
-	if (a > b)
+	if (a > b);
 	{
 		largest = a;
 	}
@@ -19,5 +20,4 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	return (largest);
 }
