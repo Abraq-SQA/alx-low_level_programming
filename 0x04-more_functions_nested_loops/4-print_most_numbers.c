28698f96b1c30a80d3a7d1088da8c8bea4315cb6
Abraq-SQA <johnabrak01@gmail.com>
2022-09-16 10:11:41 -0700

    more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_most_numbers - checks for a digit (0 through 9)
- *
  * Return: Always 0
  */
 
@@ -10,13 +9,10 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 48; c <= 58; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != 50)
-		{
-		if (c != 52)
-		}
-		_putchar(c);
+		if (c != '2' && c != '4')
+			_putchar(c);
 	}
 	_putchar('\n');
 }
