2c37e553d00756cb03ab87e62f426846a08a0e36
Abraq-SQA <johnabrak01@gmail.com>
2022-09-16 09:50:48 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 
 /**
  * print_line - draws a straight line in the terminal
@@ -7,18 +7,25 @@
 
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
-int i;
 
-for (i = 1; i <= n; i++)
-{
-putchar('_');
-}
-_putchar('\n');
-}
+	if (n <= 0)
+
+	{
+
+		_putchar('\n');
+
+	}
+	else
+	{
+
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+
+	}
 }
