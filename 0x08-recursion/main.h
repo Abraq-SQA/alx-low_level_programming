29ba97278b320802f8b79560d4cb72838fe10858
Abraq-SQA <johnabrak01@gmail.com>
2022-09-28 08:27:30 -0700

    prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,5 +15,6 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 void check(char *s, int start, int end, int *flag);
 int is_prime(int n, int start);
+int _sqrt(int n, int x)
 
 #endif /*MAIN_H*/
