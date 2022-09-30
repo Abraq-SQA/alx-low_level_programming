c25a763eee28cdbe6bcdc3c42d671d78111b6aa8
Abraq-SQA <johnabrak01@gmail.com>
2022-09-30 15:09:36 -0700

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,18 +8,13 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
+	int i;
 
-/*Declaring variables*/
-int count = 0;
-
-if (argc > 0)
-{
-/*WHILE - Print each arguments*/
-while (count < argc)
-}
-
-printf("%s\n", argv[count]);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
