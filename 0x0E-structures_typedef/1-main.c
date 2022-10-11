ad56d4e57ca8c43e6adcbe40150aface402d9bbb
Abraq-SQA <johnabrak01@gmail.com>
2022-10-11 06:48:38 -0700

    task 0

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -2,11 +2,10 @@
 #include "dog.h"
 
 /**
- * main - check the code for ALX projects.
+ * main - check the code for Holberton School students
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	struct dog my_dog;
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,8 +7,7 @@
  * @age: dog age
  * @owner: Owner
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -16,7 +15,7 @@ struct dog
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-oid print_dog(struct dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
